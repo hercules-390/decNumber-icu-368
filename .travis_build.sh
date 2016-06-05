e5c67de4e6cf7f0f7e0bb5674876ee8a0b3f5e06
Enrico Sorichetti <enricosorichetti@mac.com>
2016-06-05 17:03:27 +0200

    finding CMAKE versions available on travis

--- .travis_build.sh
+++ .travis_build.sh
@@ -1,6 +1,12 @@
 #! /usr/bin/env bash
+if [[ "$TRAVIS_OS_NAME" != "osx" ]]; then exit ; fi
+
+cmake --version
+exit
+
 rm  -rf .travis_build.dir
 mkdir   .travis_build.dir
 cd      .travis_build.dir
 cmake   ../ -DINSTALL_PREFIX=$HOME/noinst -DBUILD_TYPE=Release
 make
+exit
